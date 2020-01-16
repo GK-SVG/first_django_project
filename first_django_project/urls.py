9c1a6771e1645a43d9f1d7635f3c8e57f34ff00e
GuatamKumar777 <51847650+GK-SVG@users.noreply.github.com>
2020-01-16 21:28:07 +0530

    removed extra commet from urls.py

--- first_django_project/urls.py
+++ first_django_project/urls.py
@@ -20,20 +20,9 @@ from django.contrib import admin
 from django.urls import path 
 from . import views
 
-# Code for video 6
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', views.index, name='index'),
-#     path('about/', views.about, name='about'),
-#
-# ]
-
-# Code for video 7
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('analyze', views.analyze, name='analyze'),
    # path('ex1', views.ex1, name='ex1'),
-
-
 ]
